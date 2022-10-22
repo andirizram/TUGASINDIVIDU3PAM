9b3cd15c21bd0bcc31c78be8cde15f1dcebe011c
ANDIKARIZKI RAMADHAN <andika.120140002@student.itera.ac.id>
2022-10-22 14:09:34 +0700

    hiling

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ import React from 'react';
+ import MainPage from './src/components/layouts/MainPage';
+ import SearchPage from './src/components/layouts/SearchPage';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ 
+ const Stack = createNativeStackNavigator();
+ 
+ const App = () => {
+ 
+     return (
+         <NavigationContainer>
+             <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+                 <Stack.Screen name="MainPage" component={MainPage} />
+                 <Stack.Screen name="SearchPage" component={SearchPage} />
+             </Stack.Navigator>
+         </NavigationContainer>
+     );
+ };
+ 
+ 
+ export default App;
+ 
